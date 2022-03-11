675d27d85c045559edb1b3c5ca6258f6fab58b81
BasmaJawad <basma.jawad02@gmail.com>
2022-03-11 10:00:17 +0100

    added hello2

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,12 @@
 public class Test {
+
+
+  void hello2(){
+    System.out.println("Hey you you");
+  }
   public static void main(String[] args) {
+
+    new Test().hello2();
 
     System.out.println("Hello Basma");
     System.out.println("Hej Jonathan, Hej hej");
