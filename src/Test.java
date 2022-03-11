8f488996f33ac3a5e1a4bf52d7e4b05fdc9fc630
JWillumsen89 <100369030+JWillumsen89@users.noreply.github.com>
2022-03-11 09:53:58 +0100

    Added change to sout line 5

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,6 @@ public class Test {
   public static void main(String[] args) {
 
     System.out.println("Hello Basma");
-    System.out.println("Hej Jonathan");
+    System.out.println("Hej Jonathan, Hej hej");
   }
 }
